e90e9b16ea58fff6407dacff03e3db1a49669fb1
marfpss <mbbrpiuoficial@gmail.com>
2023-04-14 02:23:01 -0300

    .

--- commands/user/canais.js
+++ commands/user/canais.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTitle("Canais Disponíveis")
             .setDescription("Veja abaixo os canais disponíveis no site e app:")
             .addFields(
-                { name: "TV Globo", value: "https://redeglobo.globo.com/" },
+                { name: "TV Globo SP", value: "https://redeglobo.globo.com/sao-paulo/"},
                 { name: "SBT", value: "https://www.sbt.com.br/" },
                 { name: "Band", value: "https://www.band.uol.com.br/" },
                 { name: "Record", value: "https://recordtv.r7.com/" },
@@ -24,7 +24,7 @@ module.exports = {
                 { name: "ESPN", value: "https://www.espn.com.br/" },
                 { name: "ESPN 4", value: "https://www.espn.com.br/" },
                 { name: "Star Channel", value: "https://www.starplus.com/br/pt-br/" },
-                { name: "FX", value: "https://www.foxplay.com/br" },
+                { name: "FX", value: "https://www.fxnetworks.com/" },
                 { name: "Warner", value: "https://www.warnerchannel.com/br/" },
                 { name: "Cartoon", value: "https://www.cartoonnetwork.com.br/" },
                 { name: "Telecine Fun", value: "https://www.telecineplay.com.br/telecine-fun",},
@@ -34,17 +34,17 @@ module.exports = {
                 { name: "Telecine Pipoca",value: "https://www.telecineplay.com.br/telecine-pipoca",},
                 { name: "Telecine Cult", value: "https://www.telecineplay.com.br/telecine-cult",},
                 { name: "Nickelodeon", value: "https://www.nickelodeon.com.br/" },
-                { name: "Disney", value: "https://www.disneyplus.com/pt-br/" },
-                { name: "BBB Câmera Principal", value: "https://globoplay.globo.com/realities-shows/big-brother-brasil/camera-principal/",},
-                { name: "BBB 2", value: "https://globoplay.globo.com/bbb-2/p/18450/" },
-                { name: "BBB 3", value: "https://globoplay.globo.com/bbb-3/p/18452/" },
-                { name: "BBB 4", value: "https://globoplay.globo.com/bbb-4/p/18453/" },
-                { name: "BBB 5", value: "https://globoplay.globo.com/bbb-5/p/18454/" },
-                { name: "BBB 6", value: "https://globoplay.globo.com/bbb-6/p/18455/" },
-                { name: "BBB 7", value: "https://globoplay.globo.com/bbb-7/p/18456/" },
-                { name: "BBB 8", value: "https://globoplay.globo.com/bbb-8/p/18457/" },
-                { name: "BBB 9", value: "https://globoplay.globo.com/bbb-9/p/18458/" },
-                { name: "BBB 10", value: "https://globoplay.globo.com/bbb-10/p/18459/" }
+                { name: "Disney", value: "https://www.disney.com.br/tv" },
+                { name: "BBB Câmera Principal", value: "https://vitrine.globo.com/assine/bbb",},
+                { name: "BBB 2", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 3", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 4", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 5", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 6", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 7", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 8", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 9", value: "https://vitrine.globo.com/assine/bbb" },
+                { name: "BBB 10", value: "https://vitrine.globo.com/assine/bbb" }
             )
             .setColor("BLUE");
 
